250f4425f51254c1feec180128c99d4c50cc7092
Isaiah Davis <isaiah.davisx@gmail.com>
2023-12-12 00:32:06 -0600

    Fix iframe width in ServiceForm.js

--- components/ServiceForm.js
+++ components/ServiceForm.js
@@ -6,7 +6,7 @@ const ServiceForm = ({ selectedServices }) => {
       <iframe
         id="booking"
         title="Book an appointment"
-        style={{ marginRight: "-1.5em", marginLeft: "-1.5em", width: "calc(100vw - 62px)" }}
+        style={{ marginRight: "-1.5em", marginLeft: "-1.5em", width: "100%" }}
         src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ2C18CQWGd0iJPrxcw5B7lACsoutodekVzDYIDHxKwlaH9uKf5pt-4DCNPUn2MYhUad7umefWAv?gv=true"
         width={100}
         height={700}
